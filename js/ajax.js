0a9b2d6f8ec24b050f55b5a7d9b3696b39489cda
quiteee <1479425361@qq.com>
2016-12-28 18:30:04 +0800

    tb

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,56 @@
 var baseurl = "http://chuangdao.moovi-tech.com/"
 
+function register(name, password, type) {
+	var ajax = $.ajax({
+		type: "POST",
+		url: baseurl + "/register",
+		data: {
+			name: name,
+			password: password,
+			type: 1
+		}
+	});
+	return ajax;
+}
+
+function userLogin(name, password) {
+	var ajax = $.ajax({
+		type: "POST",
+		url: baseurl + "/userLogin",
+		data: {
+			name: name,
+			password: password
+		}
+	});
+	return ajax;
+}
+
+function getArticle (keyWord) {
+	var ajax = $.ajax({
+		type: "POST",
+		url: baseurl + "/getArticle",
+		data: {
+			keyWord: keyWord
+		}
+	});
+	return ajax;
+}
+
+function getCarouselList() {
+	var ajax = $.ajax({
+		url: baseurl + "/getCarouselList"
+	});
+	return ajax;
+}
+
+function getLinks() {
+	var ajax = $.ajax({
+		url: baseurl + "/getLinks"
+	});
+	return ajax;
+}
+
+
 function getIntroduction(){
 	var ajax = $.ajax({
 		url: baseurl + "/getIntroduction"
@@ -20,3 +71,39 @@ function getDevelopmentList() {
 	});
 	return ajax;
 }
+
+function getHoldingCompanyList () {
+	var ajax = $.ajax({
+		url: baseurl + "/getHoldingCompanyList",
+	});
+	return ajax; 
+}
+function getHonorList (argument) {
+	var ajax = $.ajax({
+		url: baseurl + "/getHonorList",
+	});
+	return ajax; 
+}
+
+function getPartnerList (argument) {
+	var ajax = $.ajax({
+		url: baseurl + "/getPartnerList",
+	});
+	return ajax; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
